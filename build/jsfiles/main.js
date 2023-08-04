8e551eb9894fcc5bbff0ec1500d42807b08eaea5
Uzair Ansari <uzairansari11>
2023-08-04 11:55:57 +0530

    day-1

---
+++ build/jsfiles/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let x = 5;
+let y = "hello world";
+const result = Math.round(x * 4);
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+let x: number = 5;
+let y: string = "hello world";
+
+const result: number = Math.round(x * 4);
